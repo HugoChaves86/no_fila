8c77f43acb1d48d885e3d536698772fcf364c863
hbchaves <hugo.chaves@ufba.br>
2023-11-22 01:47:07 -0300

    Ajustes

--- frontend/lib/providers.dart
+++ frontend/lib/providers.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:no_fila/pages/connect_siac.dart';
-import 'package:no_fila/widgets/auth_check.dart';
 import 'services/auth_service.dart';
 
 // Define os global providers do app
