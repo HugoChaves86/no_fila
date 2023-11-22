8242b788b2566d4933154f1162572696334309dc
wallacemendes <wallace.bmj@gmail.com>
2023-11-22 19:01:27 -0300

    set CORS permission to API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from services.siac import proof_of_registration
 from models.siac import SiacAuth, SiacProofOfRegistration
+from services.siac import proof_of_registration
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+)
+
 
 @app.post("/siac_proof_of_registration", tags=["Siac"])
-async def siac_proof_of_registration(siac_auth: SiacAuth) -> SiacProofOfRegistration:
+async def siac_proof_of_registration(
+    siac_auth: SiacAuth,
+) -> SiacProofOfRegistration:
     return await proof_of_registration(siac_auth=siac_auth)
